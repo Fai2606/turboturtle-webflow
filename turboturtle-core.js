a4d9fc26256eefebc029a836de3050ea52c53980
Fai2606 <fai@assembly.com.hk>
2026-08-28 14:50:33 +0800

    Update turboturtle-core.js

--- turboturtle-core.js
+++ turboturtle-core.js
@@ -121,6 +121,8 @@
         { sel: ".about_doggod",          from: { x: "4.6vw" }, to: { x: "0vw" }, start: "70%", end: "30%" },
         { sel: ".about_crystal",         from: { y: "15vh" },  to: { y: "0vh" }, start: "70%", end: "40%" },
         { sel: ".about_frog",            from: { y: "15vh" },  to: { y: "0vh" }, start: "70%", end: "40%" },
+        { sel: ".about_violincat",       from: { x: "3vw" }, to: { x: "0vw" }, start: "70%", end: "30%" },
+         
 
         // 城市建築全體
         { sel: ".about_citybuilding_4",  from: { y: "5vh" },   to: { y: "0vh" }, start: "100%", end: "30%" },
@@ -129,8 +131,8 @@
         { sel: ".about_citybuilding_5",  from: { y: "20vh" },  to: { y: "0vh" }, start: "100%", end: "30%" },
         { sel: ".about_citybuilding_2",  from: { y: "25vh" },  to: { y: "0vh" }, start: "100%", end: "30%" },
         { sel: ".about_backlayer",       from: { y: "30vh" },  to: { y: "0vh" }, start: "100%", end: "30%" },
-        { sel: ".about_moutain",         from: { y: "35vh" },  to: { y: "0vh" }, start: "100%", end: "30%" },
-        { sel: ".about_mountain",        from: { y: "35vh" },  to: { y: "0vh" }, start: "100%", end: "30%" }
+        { sel: ".about_mountain",        from: { y: "35vh" },  to: { y: "0vh" }, start: "100%", end: "30%" },
+        { sel: ".about_eyetower",        from: { y: "40vh" },  to: { y: "0vh" }, start: "70%", end: "30%" }
       ];
 
       cityReveals.forEach(function (item) {
