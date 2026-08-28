697f0269fd2da51f7cc56cb2ccf991637baa4efc
Fai2606 <fai@assembly.com.hk>
2026-08-28 16:05:59 +0800

    Update turboturtle-core.js

--- turboturtle-core.js
+++ turboturtle-core.js
@@ -118,8 +118,8 @@
         // 角色與特殊物件
         { sel: ".about_cityqueen",       from: { y: "35vh" },  to: { y: "0vh" }, start: "80%", end: "30%" },
         { sel: ".about_doggod",          from: { x: "4.6vw" }, to: { x: "0vw" }, start: "70%", end: "30%" },
-        { sel: ".about_crystal",         from: { y: "15vh" },  to: { y: "0vh" }, start: "70%", end: "40%" },
-        { sel: ".about_frog",            from: { y: "15vh" },  to: { y: "0vh" }, start: "70%", end: "40%" },
+        { sel: ".about_crystal",         from: { y: "-20vh" },  to: { y: "0vh" }, start: "80%", end: "40%" },
+        { sel: ".about_frog",            from: { y: "15vh" },  to: { y: "0vh" }, start: "60%", end: "40%" },
         { sel: ".about_violincat",       from: { x: "3vw" }, to: { x: "0vw" }, start: "60%", end: "30%" },
          
 
