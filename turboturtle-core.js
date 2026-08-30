c3be219534f29b8d9045a6480e2140c437e9c0c5
Fai2606 <fai@assembly.com.hk>
2026-08-30 16:27:37 +0800

    Update turboturtle-core.js

--- turboturtle-core.js
+++ turboturtle-core.js
@@ -137,18 +137,18 @@
         { sel: ".about_crystal",         from: { y: "-20vh" }, to: { y: "0vh" }, start: "100%", end: "20%" },
 
         // 城市建築全體
-        { sel: ".about_citybuilding_4",  from: { y: "5vh" },   to: { y: "0vh" }, start: "100%", end: "30%" },
-        { sel: ".about_citybuilding_3",  from: { y: "10vh" },  to: { y: "0vh" }, start: "100%", end: "30%" },
-        { sel: ".about_citybuilding_6",  from: { y: "15vh" },  to: { y: "0vh" }, start: "100%", end: "30%" },
-        { sel: ".about_citybuilding_5",  from: { y: "20vh" },  to: { y: "0vh" }, start: "100%", end: "30%" },
-        { sel: ".about_citybuilding_2",  from: { y: "25vh" },  to: { y: "0vh" }, start: "100%", end: "30%" },
-        { sel: ".about_backlayer",       from: { y: "30vh" },  to: { y: "0vh" }, start: "100%", end: "30%" },
-        { sel: ".about_mountain",        from: { y: "35vh" },  to: { y: "0vh" }, start: "100%", end: "30%" },
-        { sel: ".about_citytree_1",      from: { y: "80vh" },  to: { y: "0vh" }, start: "100%", end: "30%" },
-        { sel: ".about_citytree_2",      from: { y: "80vh" },  to: { y: "0vh" }, start: "100%", end: "30%" },
-        { sel: ".about_citytree_3",      from: { y: "80vh" },  to: { y: "0vh" }, start: "100%", end: "30%" },
-        { sel: ".about_citytree_4",      from: { y: "50vh" },  to: { y: "0vh" }, start: "100%", end: "30%" },
-        { sel: ".about_eyetower",        from: { y: "100vh" }, to: { y: "0vh" },  start: "80%", end: "30%" }
+        { sel: ".about_citybuilding_4",  from: { y: "3vh" },   to: { y: "0vh" }, start: "100%", end: "30%" },
+        { sel: ".about_citybuilding_3",  from: { y: "6vh" },  to: { y: "0vh" }, start: "100%", end: "30%" },
+        { sel: ".about_citybuilding_6",  from: { y: "9vh" },  to: { y: "0vh" }, start: "100%", end: "30%" },
+        { sel: ".about_citybuilding_5",  from: { y: "12vh" },  to: { y: "0vh" }, start: "100%", end: "30%" },
+        { sel: ".about_citybuilding_2",  from: { y: "15vh" },  to: { y: "0vh" }, start: "100%", end: "30%" },
+        { sel: ".about_backlayer",       from: { y: "18vh" },  to: { y: "0vh" }, start: "100%", end: "30%" },
+        { sel: ".about_mountain",        from: { y: "21vh" },  to: { y: "0vh" }, start: "100%", end: "30%" },
+        { sel: ".about_citytree_1",      from: { y: "60vh" },  to: { y: "0vh" }, start: "100%", end: "30%" },
+        { sel: ".about_citytree_2",      from: { y: "60vh" },  to: { y: "0vh" }, start: "100%", end: "30%" },
+        { sel: ".about_citytree_3",      from: { y: "60vh" },  to: { y: "0vh" }, start: "100%", end: "30%" },
+        { sel: ".about_citytree_4",      from: { y: "30vh" },  to: { y: "0vh" }, start: "100%", end: "30%" },
+        { sel: ".about_eyetower",        from: { y: "80vh" },  to: { y: "0vh" },  start: "82%", end: "30%" }
       ];
 
       cityReveals.forEach(function (item) {
