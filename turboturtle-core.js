535dbb5a9ed5187bd4c723e32a01cb94a64a316c
Fai2606 <fai@assembly.com.hk>
2026-09-16 01:34:49 +0800

    Update turboturtle-core.js

--- turboturtle-core.js
+++ turboturtle-core.js
@@ -682,8 +682,7 @@ if (goose) {
       { sel: ".home3_jet",     x: 90, y:  -5,  r:  0 },
       { sel: ".home3_turtle",  x: 20, y:   6,  r: -5 },
       { sel: ".home3_lion",    x: 12, y:  -3,  r: -2 },
-      { sel: ".home3_bear",    x: 5,  y:   0,  r:  0 },
-      { sel: ".home3_galaxy",  x: 0,  y: 120,  r:  0 }
+      { sel: ".home3_bear",    x: 5,  y:   0,  r:  0 }
     ];
   
     movers.forEach(function (item) {
@@ -701,6 +700,23 @@ if (goose) {
           invalidateOnRefresh: true
         }
       });
+    });
+  }
+
+  var galaxy = q(".home3_galaxy");
+
+  if (galaxy) {
+    gsap.to(galaxy, {
+      y: () => -70 * vh,
+      ease: "none",
+      force3D: true,
+      scrollTrigger: {
+        trigger: section,
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 1,
+        invalidateOnRefresh: true
+      }
     });
   }
 
