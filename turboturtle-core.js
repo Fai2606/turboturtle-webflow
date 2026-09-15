a0aaf348f689b31824f47702f6d7d82de57a9a42
Fai2606 <fai@assembly.com.hk>
2026-09-16 01:18:05 +0800

    Update turboturtle-core.js

--- turboturtle-core.js
+++ turboturtle-core.js
@@ -675,14 +675,14 @@ if (goose) {
     var movers = [
       { sel: ".home3_fish2",   x: 18, y: -4,  r:  0 },
       { sel: ".home3_fish1",   x: 24, y:  3,  r:  0 },
-      { sel: ".home3_dolphin", x: 14, y: -6,  r:  0 },
-      { sel: ".home3_shark",   x: 16, y:  4,  r:  0 },
-      { sel: ".home3_cat",     x: 22, y: -8,  r:  0 },
-      { sel: ".home3_moon",    x: 10, y:  5,  r:  0 },
-      { sel: ".home3_jet",     x: 30, y: -12, r:  0 },
-      { sel: ".home3_turtle",  x: 20, y:  6,  r:  0 },
-      { sel: ".home3_lion",    x: 12, y: -3,  r:  0 },
-      { sel: ".home3_bear",    x: 5, y:  4,  r:  0 }
+      { sel: ".home3_dolphin", x: 16, y: -6,  r:  0 },
+      { sel: ".home3_shark",   x: 12, y:  4,  r:  0 },
+      { sel: ".home3_cat",     x: 15, y: -8,  r:  0 },
+      { sel: ".home3_moon",    x: 0,  y:  20,  r:  0 },
+      { sel: ".home3_jet",     x: 30, y: -5, r:  0 },
+      { sel: ".home3_turtle",  x: 20, y:  6,  r:  -5 },
+      { sel: ".home3_lion",    x: 12, y: -3,  r:  -2 },
+      { sel: ".home3_bear",    x: 5,  y:  0,  r:  0 }
     ];
   
     movers.forEach(function (item) {
