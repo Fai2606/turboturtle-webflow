2bbe1f19e242b4121769035e11402108f84f2186
Fai2606 <fai@assembly.com.hk>
2026-09-16 12:09:31 +0800

    Update turboturtle-core.js

--- turboturtle-core.js
+++ turboturtle-core.js
@@ -751,7 +751,7 @@ if (goose) {
     
     if (section4 && section4City) {
       gsap.to(section4City, {
-        yPercent: -40,
+        yPercent: -80,
         ease: "none",
         force3D: true,
     
