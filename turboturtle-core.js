7a7f2c55d807e2aae0b05f65840a0c4cdcd9e635
Fai2606 <fai@assembly.com.hk>
2026-09-19 22:18:15 +0800

    Update turboturtle-core.js

--- turboturtle-core.js
+++ turboturtle-core.js
@@ -2113,7 +2113,7 @@ function initHomeSection6() {
 
   var lightScale =
     0.9 +
-    (0.2 * expansionProgress);
+    (0.5 * expansionProgress);
 
 
   // Smoothly chase the scroll-controlled scale
